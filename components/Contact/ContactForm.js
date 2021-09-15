49ddd4499a68610dfd579d398aecf6be6f8b878b
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-09-15 17:10:39 +0600

    notification added

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,29 +1,89 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Notification from '../UI/Notification';
 import classes from './ContactForm.module.css';
+
+const sendContactData = async (contactDetails) => {
+    const response = await fetch('/api/contact', {
+        method: 'POST',
+        body: JSON.stringify(contactDetails),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || 'Something went wrong!');
+    }
+};
 
 const ContactForm = () => {
     const [enteredEmail, setEnterdEmail] = useState('');
     const [enteredName, setEnterdName] = useState('');
     const [enteredMessage, setEnterdMessage] = useState('');
+    const [requestStatus, setRequestStatus] = useState();
+    const [requestError, setRequestError] = useState();
 
-    const submitHandler = (event) => {
+    useEffect(() => {
+        if (requestStatus === 'success' || requestStatus === 'error') {
+            const timer = setTimeout(() => {
+                setRequestStatus(null);
+                setRequestError(null);
+            }, 3000);
+
+            return () => {
+                clearTimeout(timer);
+            };
+        }
+    }, [requestStatus]);
+
+    const submitHandler = async (event) => {
         event.preventDefault();
 
-        fetch('/api/contact', {
-            method: 'POST',
-            body: JSON.stringify({
+        setRequestStatus('pending');
+
+        try {
+            await sendContactData({
                 email: enteredEmail,
                 name: enteredName,
                 message: enteredMessage,
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        setEnterdEmail('');
-        setEnterdName('');
-        setEnterdMessage('');
+            });
+            setRequestStatus('success');
+            setEnterdEmail('');
+            setEnterdName('');
+            setEnterdMessage('');
+        } catch (error) {
+            setRequestError(error.message);
+            setRequestStatus('error');
+        }
     };
+
+    let notification;
+
+    if (requestStatus === 'pending') {
+        notification = {
+            status: 'pending',
+            title: 'Sending message...',
+            message: 'Your message is on its way!',
+        };
+    }
+
+    if (requestStatus === 'success') {
+        notification = {
+            status: 'success',
+            title: 'Success!',
+            message: 'Message sent successfully!',
+        };
+    }
+
+    if (requestStatus === 'error') {
+        notification = {
+            status: 'error',
+            title: 'Error!',
+            message: requestError,
+        };
+    }
 
     return (
         <section className={classes.contact}>
@@ -70,6 +130,13 @@ const ContactForm = () => {
                     <button>Send Message</button>
                 </div>
             </form>
+            {notification && (
+                <Notification
+                    status={notification.status}
+                    title={notification.title}
+                    message={notification.message}
+                />
+            )}
         </section>
     );
 };
