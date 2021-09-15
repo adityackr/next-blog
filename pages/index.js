dda967f9a1407ec8508dc219e267501db046b054
Aditya Chakraborty <78040114+adityackr@users.noreply.github.com>
2021-09-15 17:26:32 +0600

    added head

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Fragment } from 'react';
 import FeaturedPosts from '../components/HomePage/FeaturedPosts';
 import Hero from '../components/HomePage/Hero';
@@ -6,6 +7,13 @@ import { getFeaturedPosts } from '../lib/posts-util';
 const HomePage = (props) => {
     return (
         <Fragment>
+            <Head>
+                <title>AC Blog</title>
+                <meta
+                    name="description"
+                    content="I post about programming and web development."
+                />
+            </Head>
             <Hero />
             <FeaturedPosts posts={props.posts} />
         </Fragment>
